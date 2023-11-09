3b66afb6c264f371a2dce39b105680be2448cde2
putrinaqiba <putri@putrinaqibah.com>
2023-11-09 19:32:23 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".perfume-image");
+    let currentIndex = 0;
+
+    function showImage(index) {
+        images.forEach((image, i) => {
+            if (i === index) {
+                image.classList.add("active");
+            } else {
+                image.classList.remove("active");
+            }
+        });
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    }
+
+    // Initially show the first image
+    showImage(currentIndex);
+
+    // Change the image every 20 seconds
+    setInterval(nextImage, 20000);
+});
