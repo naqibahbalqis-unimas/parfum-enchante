8d44a6bcc6b04d8c1fbe8f19baca02ddb3f7c549
putrinaqiba <putri@putrinaqibah.com>
2023-11-19 10:10:59 +0800

    Add Filtering and fix the layout by removing div row

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".perfume-image");
-    let currentIndex = 0;
-
-    function showImage(index) {
-        images.forEach((image, i) => {
-            if (i === index) {
-                image.classList.add("active");
-            } else {
-                image.classList.remove("active");
-            }
-        });
-    }
-
-    function nextImage() {
-        currentIndex = (currentIndex + 1) % images.length;
-        showImage(currentIndex);
-    }
-
-    // Initially show the first image
-    showImage(currentIndex);
-
-    // Change the image every 20 seconds
-    setInterval(nextImage, 20000);
-});
-
-
-window.addEventListener('load', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.body.classList.add('loaded');
+
+    var buttons = document.querySelectorAll(".select");
+    var cards = document.querySelectorAll(".item");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            var selected = button.name;
+            cards.forEach(card => {
+                if (selected === "all") {
+                    card.style.display = "block";
+                } else if (card.classList.contains(selected)) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+        });
+    });
 });
